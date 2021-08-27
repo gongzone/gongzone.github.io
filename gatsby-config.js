f38f1c3bfbf6e33f96db01ad28769c13a86e0c76
GongZone <dnjsqhwo@gmail.com>
2021-08-28 01:55:30 +0900

    My Blog Development start

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "GongZone DevBlog",
+  },
+  plugins: ["gatsby-plugin-styled-components"],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>sdssdsdssd안녕하세요</div>;
+};
+
+export default index;
