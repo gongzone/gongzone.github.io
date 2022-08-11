a12228f9311b884d245d1921b09b0a27241facf5
dnjsqhwo <dnjsqhwo@gmail.com>
2022-08-12 04:25:24 +0900

    ui: Tag 컴포넌트 추가

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,22 @@
+interface TagProps {
+  tagName: string;
+}
+
+export const tagColorSet = {
+  JavaScript: 'bg-yellow-400 text-black',
+  React: 'bg-blue-500 text-black',
+  Canvas: 'bg-red-700',
+  YarnBerry: 'bg-emerald-700',
+};
+
+export const Tag = ({ tagName }: TagProps) => {
+  return (
+    <span
+      className={`${
+        tagColorSet[tagName] ?? 'bg-gray-700'
+      } rounded-xl px-2 py-1 text-xs drop-shadow-lg`}
+    >
+      {tagName}
+    </span>
+  );
+};
