1bdfde94bb9d453f8e092999939ab19f37708349
dnjsqhwo <dnjsqhwo@gmail.com>
2022-08-13 23:47:06 +0900

    feat: 페이지 생성 로직 적용

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,10 +1,48 @@
-import type { Actions } from 'gatsby';
+import path from 'path';
+import type { CreateBabelConfigArgs, CreatePagesArgs } from 'gatsby';
+import { createPostPages, createPostsPages } from './gatsby-api';
 
-export const onCreateBabelConfig = ({ actions }: { actions: Actions }) => {
+const root = process.cwd();
+
+// babel config
+export const onCreateBabelConfig = ({ actions }: CreateBabelConfigArgs) => {
   actions.setBabelPreset({
     name: 'babel-preset-gatsby',
     options: {
       reactRuntime: 'automatic',
     },
   });
+};
+
+export const createPages = async ({ graphql, actions, reporter }: CreatePagesArgs) => {
+  const { createPage } = actions;
+
+  const graphQLData = await graphql(`
+    {
+      allMdx {
+        totalCount
+        nodes {
+          frontmatter {
+            slug
+            series
+          }
+        }
+      }
+    }
+  `);
+
+  if (graphQLData.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  const createPagesData = {
+    graphQLData,
+    createPage,
+    path,
+    root,
+  };
+
+  createPostPages(createPagesData);
+  createPostsPages(createPagesData);
 };
