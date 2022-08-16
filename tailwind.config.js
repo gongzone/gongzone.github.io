3094ccffd97b057a957c13e1bc13f9cb2a4c8c23
dnjsqhwo <dnjsqhwo@gmail.com>
2022-08-17 02:36:36 +0900

    chore: tailwind 설정 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/features/**/*.{js,ts,jsx,tsx}',
+    './src/templates/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     screens: {
