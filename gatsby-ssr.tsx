d8ea18b0f51e3c6f4d5fe064d24f87d1f22d6e04
dnjsqhwo <dnjsqhwo@gmail.com>
2022-08-19 15:44:19 +0900

    chore: 확장명 tsx로 변경

---
+++ gatsby-ssr.tsx
@@ -0,0 +1,12 @@
+// global style
+import './src/assets/css/global.css';
+
+// fonts
+import '@fontsource/roboto';
+import '@fontsource/noto-sans-kr';
+import '@fontsource/poor-story';
+
+// gatsby-api
+import { wrapRootWithMdx } from './gatsby-api/wrap-root-with-mdx';
+
+export const wrapRootElement = wrapRootWithMdx;
