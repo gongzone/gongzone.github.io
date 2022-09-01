5ebf1a1271daa6926988e79db04d2dfd928bf4a4
dnjsqhwo <dnjsqhwo@gmail.com>
2022-09-02 04:56:55 +0900

    ui: 헤더 컴포넌트 padding 및 gap 조절

--- src/components/header/atoms/header-nav-link.tsx
+++ src/components/header/atoms/header-nav-link.tsx
@@ -10,7 +10,7 @@ export const HeaderNavLink = ({ navItem }: HeaderNavLinkProps) => {
   return (
     <Link className="hover-text-amber flex items-center gap-2 p-2 text-zinc-300" to={navItem.to}>
       <span className="text-xl">{<navItem.icon />}</span>
-      <span className="text-lg font-bold">{navItem.name}</span>
+      <span className="text-base font-bold">{navItem.name}</span>
     </Link>
   );
 };
