ac7d22a0975a889510877221d075dbb6e540260e
dnjsqhwo <dnjsqhwo@gmail.com>
2022-09-07 21:10:42 +0900

    chore: 개발중인 페이지를 임시로 나타내는 under-development 컴포넌트 생성

---
+++ src/components/under-development/under-development.tsx
@@ -0,0 +1,9 @@
+import { BaseLayout } from '@/layout/base-layout';
+
+export const UnderDevelopment = () => {
+  return (
+    <BaseLayout className="min-h-[calc(100vh-(172px+112px))]">
+      <span>underDevelopment</span>
+    </BaseLayout>
+  );
+};
