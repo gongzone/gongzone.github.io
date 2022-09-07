8a81a1bd87e3a59788812266ee70adf8bf7a12fb
dnjsqhwo <dnjsqhwo@gmail.com>
2022-09-07 22:05:08 +0900

    ui: 중앙에 위치하도록 조정

--- src/components/under-development/under-development.tsx
+++ src/components/under-development/under-development.tsx
@@ -2,8 +2,11 @@ import { BaseLayout } from '@/layout/base-layout';
 
 export const UnderDevelopment = () => {
   return (
-    <BaseLayout className="min-h-[calc(100vh-(172px+112px))]">
-      <span>underDevelopment</span>
+    <BaseLayout className="relative min-h-[calc(100vh-(172px+112px))]">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+        <h2 className="text-xl">현재 페이지는 개발 중입니다.</h2>
+        <span>(Under Development)</span>
+      </div>
     </BaseLayout>
   );
 };
