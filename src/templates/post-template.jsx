7b90bfd62e561c954ff182ae705b56816ee9810a
dnjsqhwo <dnjsqhwo@gmail.com>
2022-09-16 22:35:27 +0900

    ui: 대표 이미지 중앙으로 오도록 조정

--- src/templates/post-template.jsx
+++ src/templates/post-template.jsx
@@ -66,7 +66,7 @@ const PostTemplate = ({ data, pageContext, children }) => {
       <div className="relative mb-4">
         <Toc tableOfContents={tableOfContents} />
         <div>
-          <div>
+          <div className="flex items-center justify-center">
             <GatsbyImage className="rounded-lg" image={getImage(image)} alt={title} />
           </div>
 
