31ec5cccf06f54efee5d4df741cabc5e1772df14
dnjsqhwo <dnjsqhwo@gmail.com>
2022-09-17 05:57:12 +0900

    fix: drop-shadow가 일으키는 페이지 속도 저하 문제 해결

--- src/layout/grid-layout.tsx
+++ src/layout/grid-layout.tsx
@@ -6,7 +6,7 @@ interface GridLayoutProps {
 
 export const GridLayout = ({ children }: GridLayoutProps) => {
   return (
-    <ul className="grid w-full grid-cols-1 gap-5 drop-shadow-lg 3xl:gap-8 sm:grid-cols-2 md:gap-7 lg:grid-cols-3 xl:grid-cols-4">
+    <ul className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 md:gap-7 lg:grid-cols-3 xl:grid-cols-4 3xl:gap-8">
       {children}
     </ul>
   );
